760e773d5e6c1d7fe5995f93f952098576bc0d0f
Sarah Payne <sarah@Sarahs-MacBook-Pro.local>
2017-05-15 23:03:58 -0500

    put characters in one array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,54 +1,67 @@
 $(document).ready(function() {
 
-var micheal = {
-	healthPoints:100,
-	attackPower: 20,
-	counterAttackPower: 10
-}
-var dwight = {
-	healthPoints:160,
-	attackPower: 40,
-	counterAttackPower: 28
-}
-var jim = {
-	healthPoints:180,
-	attackPower: 60,
-	counterAttackPower: 18
-}
-var andy = {
-	healthPoints: 80,
-	attackPower: 10,
-	counterAttackPower: 5
-}
+var characters = [
+   {    
+    name: 'Micheal',
+    healthPoints: 100,
+    attackPower: 20,
+    counterAttackPower: 10,
+    imgSource: 'http://placehold.it/100x100'
+    },
+    {
+    name: 'Dwight',
+    healthPoints: 160,
+    attackPower: 40,
+    counterAttackPower: 28,
+    imgSource: 'http://placehold.it/101x100'
+    },
+    {
+    name: 'Jim',
+    healthPoints: 180,
+    attackPower: 60,
+    counterAttackPower: 10,
+    imgSource: 'http://placehold.it/102x100'
+    },
+    {
+    name: 'Andy',
+    healthPoints: 80,
+    attackPower: 10,
+    counterAttackPower: 5,
+    imgSource: 'http://placehold.it/103x100'	
+    }];
+
+var potentialEnemies = [];
+var currentEnemy = {};
+var selectedCharacter = {};
 
 var activeCharacter = false;
 var activeEnemy = false;
-var passiveEnemy
 
+$('.img').on("click", function () {
+	$('#enemies-available').append($('[data-char="inactive"]'));
+	$('#chosen-character').append(this);
+	$(this).attr("data-char","active");
+	$('#first-instruction').hide();
+	$('#second-instruction').css("display","block");
+	activeCharacter = true;
+	selectEnemy();
+});
 
+$('.img').on("click", function () {
+		$('#chosen-enemy').append(this);
+		$(this).attr("data-char","active");
+		$('#second-instruction').hide();
+		$('#third-instruction').css("display","block");
+		activeEnemy = true;
+});
 
 function selectCharacter () {
 	if (activeCharacter===false && activeEnemy===false) {
-		$('.img').on("click", function () {
-			$('#enemies-available').append($('[data-char="inactive"]'));
-			$('#chosen-character').append(this);
-			$(this).attr("data-char","active");
-			$('#first-instruction').hide();
-			$('#second-instruction').css("display","block")
-			activeCharacter = true;
-			selectEnemy();
-	
-})}};
+	};
+};
 
 function selectEnemy () {
-	if (activeCharacter===true && activeEnemy==false) {
-		$('.img').on("click", function () {
-			$('#chosen-enemy').append(this);
-			$(this).attr("data-char","active")
-			$('#second-instruction').hide();
-			$('#third-instruction').css("display","block")
-			activeEnemy = true;
-		});
+	if (activeCharacter===true && activeEnemy===false) {
 	};
 };
 
