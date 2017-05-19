1d0f7e75123cd9006222c6fd908448a46b18c9ec
Sarah Payne <sarah@Sarahs-MacBook-Pro.local>
2017-05-18 23:24:47 -0500

    almost fully functioning game, hp shows up on css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,28 +3,28 @@ var characters = [
    {    
     name: 'Micheal',
     healthPoints: 100,
-    attackPower: 20,
-    counterAttackPower: 10,
+    attackPower: 5,
+    counterAttackPower: 5,
     imgSource: 'http://placehold.it/100x100'
     },
     {
     name: 'Dwight',
-    healthPoints: 160,
-    attackPower: 40,
-    counterAttackPower: 28,
+    healthPoints: 150,
+    attackPower: 15,
+    counterAttackPower: 20,
     imgSource: 'http://placehold.it/101x100'
     },
     {
     name: 'Pam',
     healthPoints: 180,
-    attackPower: 60,
-    counterAttackPower: 10,
+    attackPower: 20,
+    counterAttackPower: 25,
     imgSource: 'http://placehold.it/102x100'
     },
     {
     name: 'Jim',
-    healthPoints: 80,
-    attackPower: 10,
+    healthPoints: 120,
+    attackPower: 8,
     counterAttackPower: 5,
     imgSource: 'http://placehold.it/103x100'	
     }];
@@ -32,19 +32,32 @@ var characters = [
 var potentialEnemies = [];
 var currentEnemy = {};
 var selectedCharacter = {};
-
 var activeCharacter = false;
 var activeEnemy = false;
+var deadEnemies = 0;
 
 function endGame() {
 	if (selectedCharacter.healthPoints<=0) {
 		alert("You lose, sucker! Press 'Restart' to try again.")
 		return;
-	} else if (currentEnemy.healthPoints<=0) {
-
+	} if (currentEnemy.healthPoints<=0) {
+		$("#fourth-instruction").css("display","block")
+		$('#score').html("");
+		$('#score2').html("");
+		$('#chosen-enemy').html("");
+		activeEnemy = false;
+		currentEnemy = {};
+		deadEnemies++;
+	} if (deadEnemies==3) {
+			alert("You win!");
+			$("#fourth-instruction").hide();
 	}
-
 };
+
+$("#one").html(characters[0].healthPoints)
+$("#two").html(characters[1].healthPoints)
+$("#three").html(characters[2].healthPoints)
+$("#four").html(characters[3].healthPoints)
 
 ($('[data-name="michael"]')).on("click", function () {
 	if (activeCharacter==false && activeEnemy==false) {
@@ -63,6 +76,8 @@ function endGame() {
 		$(this).attr("data-char","active");
 		$('#second-instruction').hide();
 		$('#third-instruction').css("display","block");
+		$("#fourth-instruction").hide();
+		$("#fifth-instruction").hide();
 		currentEnemy = characters[0]
 		activeEnemy = true;
 
@@ -86,6 +101,8 @@ function endGame() {
 		$(this).attr("data-char","active");
 		$('#second-instruction').hide();
 		$('#third-instruction').css("display","block");
+		$("#fourth-instruction").hide();
+		$("#fifth-instruction").hide();
 		currentEnemy = characters[1]
 		activeEnemy = true;
 
@@ -109,6 +126,8 @@ function endGame() {
 		$(this).attr("data-char","active");
 		$('#second-instruction').hide();
 		$('#third-instruction').css("display","block");
+		$("#fourth-instruction").hide();
+		$("#fifth-instruction").hide();
 		currentEnemy = characters[2]
 		activeEnemy = true;
 
@@ -132,38 +151,65 @@ function endGame() {
 		$(this).attr("data-char","active");
 		$('#second-instruction').hide();
 		$('#third-instruction').css("display","block");
+		$("#fourth-instruction").hide();
+		$("#fifth-instruction").hide();
 		currentEnemy = characters[3]
 		activeEnemy = true;
 	};
 });
 
 $('#fight').on("click", function (){
-   $('#score').html("You attacked " + currentEnemy.name +" for " + selectedCharacter.attackPower + " damage! <br> " +
+	if (activeEnemy==true) {
+   	$('#score').html("You attacked " + currentEnemy.name +" for " + selectedCharacter.attackPower + " damage! <br> " +
    	currentEnemy.name + " attacked you back for " + currentEnemy.counterAttackPower + " damage!");
 
-   selectedCharacter.healthPoints = selectedCharacter.healthPoints - currentEnemy.counterAttackPower;
-   currentEnemy.healthPoints = currentEnemy.healthPoints - selectedCharacter.attackPower;
-   selectedCharacter.attackPower = selectedCharacter.attackPower * 2;
+	selectedCharacter.healthPoints = selectedCharacter.healthPoints - currentEnemy.counterAttackPower;
+	currentEnemy.healthPoints = currentEnemy.healthPoints - selectedCharacter.attackPower;
+	selectedCharacter.attackPower = selectedCharacter.attackPower * 2;
    
-   $('#score2').html("Your health points: " + selectedCharacter.healthPoints + "<br>" +
+	$('#score2').html("Your health points: " + selectedCharacter.healthPoints + "<br>" +
    	currentEnemy.name + "'s health points: " + currentEnemy.healthPoints);
 
+	$("#one").html(characters[0].healthPoints)
+	$("#two").html(characters[1].healthPoints)
+	$("#three").html(characters[2].healthPoints)
+	$("#four").html(characters[3].healthPoints)
+
    endGame();
+
+	} 
 });
 
-$('restart').on("click", function () {
+$('#restart').on("click", function () {
+	var currentEnemy = {};
+	var selectedCharacter = {};
+	var activeCharacter = false;
+	var activeEnemy = false;
+	var deadEnemies = 0;
+	$('#score').html("");
+	$('#score2').html("");
+	$('#first-instruction').css("display","block")
+	$('#second-instruction').hide();
+	$('#third-instruction').hide();
+	$("#fourth-instruction").hide();
+	$("#fifth-instruction").hide();
+	$('#character-container').append($('.image-with-float'));
+	$('#enemies-available').html("");
+	$('#chosen-character').html("");
+	$('#chosen-enemy').html("");
+	//not working!//
 });
 
 
 
-//Have P's show up during game // 
 
-// win and loss based on points
 
 // reset button//
 
 // add media queries //
 
+
+// maybe change the colors of the floats behind the images like in the example //
 
 
 
